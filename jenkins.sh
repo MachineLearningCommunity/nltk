b1db643459e3d09f0dc2b74dc405873dcb1ed884
alvations <alvations@gmail.com>
2018-01-21 00:18:34 +0800

    Added CoreNLP server start and kill to jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -23,7 +23,18 @@ if [[ ! -d ${stanford_corenlp_package_name} ]]; then
 	unzip ${stanford_corenlp_package_zip_name}
 	rm ${stanford_corenlp_package_zip_name}
 	ln -sf ${stanford_corenlp_package_name} 'stanford-corenlp'
+	# Kill all Java instances.
+	pkill -f '*edu.stanford.nlp.pipeline.StanfordCoreNLPServer*'
+	cd stanford-corenlp
+	# Start the CoreNLP server
+	nohup java -Xmx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer \
+	-preload tokenize,ssplit,pos,parse \
+	-status_port 9005  -port 9005 -timeout 15000 &
+	# Log the job ID and kill it before the end.
+	CORENLP_PID=$!
+	cd ..
 fi
+
 
 #stanford_parser_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/lex-parser.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
 stanford_parser_package_zip_name="stanford-parser-full-2017-06-09.zip"
@@ -85,6 +96,7 @@ iconv -c -f utf-8 -t utf-8 nosetests.xml > nosetests_scrubbed.xml
 touch ~/.pylintrc
 pylint -f parseable nltk > pylintoutput
 
+kill -9 $CORENLP_PID
 
 #script always succeeds
 true
